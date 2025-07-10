5ed74db68d41a4223a3a184185d90a449eaa7113
rhythmm16 <rhythm161203@gmail.com>
2025-07-10 15:28:04 +0530

    CallBack

---
+++ Lecture4/index.js
@@ -0,0 +1,62 @@
+let account_balance=200000;
+let products=[{
+    name:"samsung",
+    amount:70000,
+    quantity:10
+},{
+    name:"iPhone 14",   
+    amount:100000,
+    quantity:1
+}]
+
+// function buyProduct(product_name,callback) {
+//     //some async operation
+//     //1. get product detail from product db
+//     //2. write order detail in user db
+//     setTimeout(() => {
+//         console.log(`Product bought successfully!`)
+//         callback();
+//     })
+// }
+
+
+//console.log("Order placed for iPhone 14 Pro Max")
+function buyProduct(product_name,callback) {
+    let isprodcut=null;
+   //implement for loop to find product in an array
+   //find product object from products array whose name is equal to product_name
+   for(let i=0;i<products.length;i++){
+        console.log(products[i].name==product_name);
+        if(products[i].name==product_name){
+            isprodcut=products[i];
+        }
+    }
+if(!isprodcut){
+    callback("Product not available",null);
+}else{
+    callback(null,isprodcut.amount);
+}
+
+function (amount,callback) 
+    if(amount>account_balance){
+        callback("Insufficient balance",null);
+    }      else{
+        account_balance-=amount;
+        callback(null,"your product has been purchased successfully");
+    }
+}
+
+buyProduct("iphone 14",function(err,amount){
+    // console.log("Order placed for iPhone 14 Pro Max");
+    if(err) return console.log(err);
+    console.log(amount)
+    deductAmount(amount,function(err,message){
+        if(err) return console.log(err);
+        console.log(message)
+        console.log(account_balance);
+    });
+})
+
+//problems in callback
+//1. Callback hell- nested callbacks, therefore code readability is reduced
+//2. Don't have control over the flow of execution
