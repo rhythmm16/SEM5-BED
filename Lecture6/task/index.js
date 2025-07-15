8fbf722af6244686e9f09c56d71786e012e05607
rhythmm16 <rhythm161203@gmail.com>
2025-07-15 10:38:22 +0530

    lecture6

---
+++ Lecture6/task/index.js
@@ -0,0 +1,16 @@
+fs=require("fs");
+//read demo.txt and demo2.txt and wrtie in file result.txt
+
+fs.readFile("../demo.txt","utf-8",function(err,data){
+    if(err)return console.log(err);
+    let data1=data;
+    fs.readFile("../demo2.txt","utf-8",function(err,data){
+        if(err)return console.log(err);
+        let data2=data;
+        let result=data1.trim()+"\n"+data2;
+        fs.writeFile("./result.txt",result,function(err){
+            if(err)return console.log(err);
+            console.log("done");
+        })
+    })
+})
---
+++ Lecture6/write/index.js
@@ -0,0 +1,6 @@
+const fs=require('fs');
+
+fs.writeFile("../demo2.txt","hello world",function(err,data){
+    if(err)return console.log(err);
+    console.log("success");
+})
